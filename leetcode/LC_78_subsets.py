e1c7ebb5159816ac0e6a19ebfa125cd7389fcf1b
matt meng <ymeng.ucla@gmail.com>
2016-04-25 12:32:00 -0700

    new problems

--- leetcode/LC_78_subsets.py
+++ leetcode/LC_78_subsets.py
@@ -18,7 +18,7 @@ class Solution:
 		finalRes = []
 		for elem in resList:
 			if len(elem)!=0:
-				#print 'the elem: ', elem				
+				#print 'the elem: ', elem
 				elem.sort()
 				#print tmp
 				resSet.add(tuple(elem))
@@ -29,8 +29,31 @@ class Solution:
 		return finalRes
 
 
+'''
+different bitwise operation to create the full list
+'''
+tot_num_elem = 5
+total = 1 << tot_num_elem # this is the all the possible combiations, represented by bits 1111 for 4 elements
+
+for i in range(total):
+    tmp = []
+    t = i
+    j = 0
+    for j in range(tot_num_elem): # loop through all the bits/elements to assign elements into the bit location
+        if (t==0):
+            break
+
+        if (t & 1):
+            tmp.append(j)
+
+        t = t >> 1
+    print i, tmp
+
+
+
+
 solut = Solution()
 nums = [1, 2, 3]
 nums = [4,1,0]
 nums = [1, 2, 2]
-print solut.subsets(nums)	 
+print solut.subsets(nums)
