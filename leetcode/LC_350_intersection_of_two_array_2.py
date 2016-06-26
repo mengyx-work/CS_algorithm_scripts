108b8095a6594db987f3e7dda1ad7cb9a58b8927
matt meng <ymeng.ucla@gmail.com>
2016-06-25 19:54:10 -0700

    LC#350

---
+++ leetcode/LC_350_intersection_of_two_array_2.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if len(nums1) < len(nums2):
+            short_list = nums1
+            long_list = nums2
+        else:
+            long_list = nums1
+            short_list = nums2
+
+        common_list = []
+        for elem in short_list:
+            if elem in long_list:
+                common_list.append(elem)
+                long_list.remove(elem)
+
+        return common_list
+
+
+
+solv = Solution()
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+print solv.intersect(nums1, nums2)
+
+        
+        
