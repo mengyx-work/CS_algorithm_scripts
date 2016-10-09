07cf400d778688ff73b26554999612b768447608
mengyx-work <ymeng.ucla@gmail.com>
2016-10-08 22:37:32 -0700

    LC#290

---
+++ leetcode/LC_290_word_pattern.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        patternDic = {}
+        pattern = list(pattern)
+        words = str.split(' ')
+
+        if len(pattern) != len(words):
+            return False
+
+        for i, elem in enumerate(pattern):
+            if elem not in patternDic:
+                if words[i] in patternDic.values():
+                    return False
+                else:
+                    patternDic[elem] = words[i]
+            else:
+                if patternDic[elem] != words[i]:
+                    return False
+
+        return True
+
+sol = Solution()
+print sol.wordPattern('abba', 'dog cat cat dog')
+print sol.wordPattern('abba', 'dog dog dog dog')
+print sol.wordPattern('abaa', 'dog cat cat dog')
+
+        
