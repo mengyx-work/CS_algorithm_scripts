5448c4261d6a003c2191a01917f283dde995d915
Matt Meng <ymeng.ucla@gmail.com>
2017-07-28 18:29:36 -0700

    LC#392

--- leetcode/LC_392_Is_Subsequence.py
+++ leetcode/LC_392_Is_Subsequence.py
@@ -1,4 +1,17 @@
 class Solution(object):
+    def isSubsequence(self, s, t):
+        if len(s) == 0:
+            return True
+        cur_index = 0
+        tot_count = len(list(s))
+        for char in list(t):
+            if char == s[cur_index]:
+                cur_index += 1
+                if cur_index == tot_count:
+                    return True
+        return False
+
+    '''
     def _find_upper_index(self, indexs, cur_index):
         lb, ub = 0, len(indexs) - 1
         while(lb + 1 < ub):
@@ -13,18 +26,15 @@ class Solution(object):
             return indexs[ub]
         return -1
 
-
     def isSubsequence(self, s, t):
+        if len(s) == 0:
+            return True
         word_dict = {}
         for i, chr in enumerate(t):
             if chr not in word_dict:
                 word_dict[chr] = [i]
             else:
                 word_dict[chr].append(i)
-
-        if len(s) == 0:
-            return False
-
         cur_index = -1
         for chr in s:
             if chr not in word_dict:
@@ -34,8 +44,8 @@ class Solution(object):
             if found_index == -1:
                 return False
             cur_index = found_index
-
         return True
+        '''
 
 sol = Solution()
 s = "abc"
