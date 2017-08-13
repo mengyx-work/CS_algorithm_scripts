2024b828d7eb1e69a5a15b3d57c1326dd374fdb7
Matt Meng <ymeng.ucla@gmail.com>
2017-08-12 21:36:44 -0700

    LC#300

--- leetcode/LC_300_longest_increasing_subsequence.py
+++ leetcode/LC_300_longest_increasing_subsequence.py
@@ -1,5 +1,25 @@
 class Solution(object):
     def lengthOfLIS(self, nums):
+        max_counts = []
+        if len(nums) == 0:
+            return 0
+        for i, num in enumerate(nums):
+            max_count = 1
+            for j in xrange(i-1, -1, -1):
+                if nums[j] < num:
+                    max_count = max(max_count, max_counts[j]+1)
+            max_counts.append(max_count)
+        return max(max_counts)
+
+sol = Solution()
+nums = [1,3,6,7,9,4,10,5,6]
+assert sol.lengthOfLIS(nums) == 6
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+assert sol.lengthOfLIS(nums) == 4
+
+'''
+    # DP solution, build a monotonic array to represent the solution
+    def lengthOfLIS(self, nums):
         size = 0
         min_values = []
         for num in nums:
@@ -16,3 +36,4 @@ class Solution(object):
                 min_values[i] = num
             size = max(i+1, size)
         return size
+'''
