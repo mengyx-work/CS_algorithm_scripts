dc77b569b227875a869ea0a1dd5dd65fb7430d63
Matt Meng <ymeng.ucla@gmail.com>
2017-08-13 22:17:15 -0700

    LC#16

--- leetcode/LC_16_3Sum_Closet.py
+++ leetcode/LC_16_3Sum_Closet.py
@@ -1,38 +1,40 @@
+import sys
 class Solution:
-    # @param {integer[]} nums
-    # @param {integer} target
-    # @return {integer}
+    def _find_mind_diff(self, nums, start_idx, target):
+        min_diff, best_sum = sys.maxint, 0
+        lo, hi = start_idx, len(nums) - 1
+        while lo < hi:
+            cur_diff = target - nums[lo] - nums[hi]
+            if abs(cur_diff) < min_diff:
+                min_diff = abs(cur_diff)
+                best_sum = nums[lo] + nums[hi]
+            if cur_diff > 0:
+                lo += 1
+            else:
+                hi -= 1
+        return min_diff, best_sum
+
     def threeSumClosest(self, nums, target):
+        if len(nums) <= 3:
+            return sum(nums)
         nums.sort()
-        res = [2**30, (0, 0, 0)]
-        for i in range(len(nums)-2):
-            aVal = nums[i]
-            bIndx = i + 1
-            cIndx = len(nums) - 1
+        min_diff, closest_sum  = sys.maxint, 0
+        for i in xrange(len(nums)-2):
+            cur_min_diff, best_sum = self._find_mind_diff(nums, i+1, target-nums[i])
+            if cur_min_diff < min_diff:
+                min_diff = cur_min_diff
+                closest_sum = nums[i] + best_sum
+        return closest_sum
 
-            while bIndx<cIndx:
-                diff = aVal + nums[bIndx] + nums[cIndx] - target
-                print 'diff: %i, res[0]: %i' % (diff, res[0])
-                print aVal, nums[bIndx], nums[cIndx]
-                if abs(diff)<=res[0]:
-                    res[1] = (aVal, nums[bIndx], nums[cIndx])
-                    res[0] = abs(diff)
-                if diff>0:
-                    cIndx -= 1
-                elif diff<0:
-                    bIndx += 1
-                else:
-                    return target
-        
-        return sum(res[1])
-
-
-
-solut = Solution()
+sol = Solution()
+data = [0,2,1,-3]
+data.sort()
+#print sol._find_mind_diff(data, 1, 4)
+assert sol.threeSumClosest(data, 1) == 0
+#print sol._find_mind_diff(data, 0, -100)
 data = [-1, 2, 1, -4]
-data = [0 ,1, 2]
-data = [1 ,1, 1, 0]
-print solut.threeSumClosest(data, -100)
+assert sol.threeSumClosest(data, 1) == 2
+
                 
 
 
--- leetcode/LC_18_4sum.py
+++ leetcode/LC_18_4sum.py
@@ -1,59 +1,60 @@
 class Solution:
-    def fourSum(self, nums, target):
-        if len(nums)<4:
+    def _find_match_pair(self, nums, start_idx, target):
+        if start_idx >= len(nums) - 1:
             return []
-
-        SumDict = {}
-        res = set([])
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                Sum = nums[i] + nums[j]
-                #SumDict.append((Sum, (i, j)))
-                if Sum in SumDict:
-                    SumDict[Sum].append((i, j))
-                else:
-                    SumDict[Sum] = [(i, j)]
-    
-        sortList = sorted(SumDict.items(), key=lambda x: x[0])
-        start = 0
-        end = len(sortList) - 1
-        #print sortList
-
-        while start<=end:
-            if (sortList[start][0] + sortList[end][0]) == target:
-                for aSet in sortList[start][1]:
-                    for bSet in sortList[end][1]:
-                        unit = set([])
-                        unit.add(aSet[0])
-                        unit.add(aSet[1])
-                        unit.add(bSet[0])
-                        unit.add(bSet[1])
-                        #print 'index: ', aSet[0], aSet[1], bSet[0], bSet[1]
-                
-                        if len(unit)==4:
-                            unit = [ nums[indx] for indx in unit]
-                            unit.sort()
-                            #print 'unit result: ', unit
-                            res.add(tuple(unit))
-
-                start += 1
-                end -= 1
+        lo, hi = start_idx, len(nums) - 1
+        pairs = []
+        while lo < hi:
+            if nums[lo] + nums[hi] == target:
+                pairs.append([nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
                 continue
+            if nums[lo] + nums[hi] > target:
+                hi -= 1
+                #while hi > lo and nums[hi+1] == nums[hi]:
+                #    hi -= 1
+            else:
+                lo += 1
+                #while lo < hi and  nums[lo] == nums[lo-1]:
+                #    lo += 1
+        return pairs
 
-            if (sortList[start][0] + sortList[end][0])>target:
-                end -= 1
+    def fourSum(self, nums, target):
+        nums.sort()
+        if len(nums) < 4:
+            return []
+        results = set()
+        prev_i, prev_j = None, None
+        for i in xrange(len(nums)-3):
+            for j in xrange(i+1, len(nums)-2):
+                #if nums[i] == nums[j] or nums[i] == prev_i or nums[j] == prev_j:
+                #    continue
+                prev_i, prev_j = nums[i], nums[j]
+                pair_target = target - prev_i - prev_j
+                # the starting third element nums[j+1] is different from nums[j]
+                #while j+1 < len(nums) and nums[j+1] == nums[j]: 
+                #    j += 1
+                #if j+1 == len(nums)-1:
+                #    continue
+                pairs = self._find_match_pair(nums, j+1, pair_target)
+                if pairs:
+                    for pair in pairs:
+                        results.add((prev_i, prev_j, pair[0], pair[1]))
+        return [list(elem) for elem in results]
 
-            if (sortList[start][0] + sortList[end][0])<target:
-                start += 1
+         
 
-        return list(res)
-
-
-solut = Solution()
-#data = [-1, 2, 1, -4]
-#data = [0 ,1, 2]
-#data = [1, 0, -1, 0, -2, 2]
-data = [0, 0, 0, 0]
-data = [-3,-1,0,2,4,5]
-data = [-3,-2,-1,0,0,1,2,3]
-print solut.fourSum(data, 0)
+sol = Solution()
+nums = [-3,-2,-1,0,0,1,2,3]
+print sol.fourSum(nums, 0)
+assert sol.fourSum(nums, 0) == [[-3,-2,2,3],[-3,-1,1,3],[-3,0,0,3],[-3,0,1,2],[-2,-1,0,3],[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+'''
+# none-dupplicate test cases
+nums = [-3,-1,0,2,4,5]
+assert sol.fourSum(nums, 0) == [[-3, -1, 0, 4]]
+nums = [0, 0, 0, 0]
+assert sol.fourSum(nums, 0) == []
+nums = [-3,-2,-1,0,0,1,2,3]
+assert sol.fourSum(nums, 0) ==  [[-3, -2, 2, 3], [-2, -1, 0, 3], [-1, 0, 0, 1]]
+'''
