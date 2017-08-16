0df8b4dc667caf5026cf2e29fd19bb27db25e6a5
Matt Meng <ymeng.ucla@gmail.com>
2017-08-15 17:50:00 -0700

    LC#46 update

--- leetcode/LC_46_permutation.py
+++ leetcode/LC_46_permutation.py
@@ -17,61 +17,7 @@ class Solution:
                         tmp.append(num)
                         stack.append(tmp)
         return res
-
-    ###########################
-    def permute(self, nums):
-        if len(nums)==1:
-            return [[nums[0]]]
-        stack = []
-        res = []
-        for num in nums:
-            tmpNums = nums[:]
-            tmpNums.remove(num)
-            stack.append(([num], tmpNums))
-
-        print stack
-        while stack:
-            v= stack.pop()
-            print 'v: ', v
-            if len(v[1])==1:
-                v[0].append(v[1][0])
-                res.append(v[0])
-            else:
-                for num in v[1]:
-                    tmpNums = v[1][:]
-                    tmpList = v[0][:]
-                    tmpList.append(num)
-                    tmpNums.remove(num)
-                    stack.append((tmpList, tmpNums))
-
-        return res
-        '''
-	def creatPermut(self, nums, curIndx, maxIndx, res):
-		#print 'the first nums: ', nums, 'curIndx: ', curIndx
-		if curIndx==maxIndx:
-            res.append(tuple(nums))
-            #res.append(nums)            
-            #print 'the result: ', res
-            return
-       
-        for i in range(curIndx, maxIndx):
-			visited[nums[i]] = 1    
-            tmp1 = nums[curIndx]
-            nums[curIndx] = nums[i]
-            nums[i] = tmp1
-            #print 'nums: ', nums 
-            self.creatPermut(nums, (curIndx+1), maxIndx, res)
-            
-            tmp1 = nums[curIndx]
-            nums[curIndx] = nums[i]
-            nums[i] = tmp1
-
-    def permute(self, nums):
-        resList = []
-        self.creatPermut(nums, 0, len(nums), resList)
-        #print 'the result to send: ', resList
-        tmpRes = [list(elem) for elem in resList]
-        return tmpRes
+    '''
 
 solut = Solution()
 #nums = []
