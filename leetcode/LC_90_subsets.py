29283f04755199c576a0d389ff78d132b23d836a
Matt Meng <ymeng.ucla@gmail.com>
2017-08-15 17:09:43 -0700

    LC#90

--- leetcode/LC_90_subsets.py
+++ leetcode/LC_90_subsets.py
@@ -1,5 +1,35 @@
 class Solution:
-    '''
+    def _add_element(self, nums, start_idx, cur_result, results):
+        results.append(cur_result)
+        if start_idx >= len(nums):
+            return
+        prev_idx = None
+        for i in xrange(start_idx, len(nums)):
+            tmp_result = cur_result[:]
+            tmp_result.append(nums[i])
+            if prev_idx is not None and nums[i] == nums[prev_idx]:
+                continue
+            else:
+                self._add_element(nums, i+1, tmp_result, results)
+                prev_idx = i
+
+    def subsetsWithDup(self, nums):
+        if len(nums) == 0:
+            return []
+        nums.sort()
+        results = []
+        self._add_element(nums, 0, [], results)
+        return results
+
+sol = Solution()
+nums = [1, 2, 3]
+#nums = [4,1,0]
+#nums = [1, 2, 2]
+#nums = [1, 5, 5, 5]
+nums = [4,4,4,1,4]
+assert sol.subsetsWithDup(nums) == [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+
+'''
     def subsetsWithDup(self, S):
         res = [[]]
         S.sort()
@@ -10,7 +40,6 @@ class Solution:
             for j in range(len(res) - l, len(res)):
                 res.append(res[j] + [S[i]])
         return res
-    '''
 
     def subsetsWithDup(self, nums):
         nums.sort()
@@ -31,23 +60,7 @@ class Solution:
             #print 'tmpList: ', tmpList, 'resList: ', resList
                 self.dfs(nums, i+1, tmpList, resList)
                 tmpList.pop()
+'''
 
-    '''
-    def subsetsWithoutDup(self, nums):    
-        nums.sort()
-        resList = [[]]
-        for elem in nums:
-            print 'the resList: ', resList
-            tmpList = resList[:]
-            for tmp in tmpList:
-                resList.append(tmp+[elem])
 
-        return resList
-    '''
 
-solut = Solution()
-nums = [1, 2, 3]
-#nums = [4,1,0]
-#nums = [1, 2, 2]
-nums = [1, 5, 5, 5]
-print solut.subsetsWithDup(nums)
